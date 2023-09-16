98b425d787f949dae701ad3fb9443b3608f460b8
Nathan GNANKADJA <gnankadjanathan@gmail.com>
2023-09-16 20:27:16 +0100

    config: [#2] setup conventional commit

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -29,7 +29,7 @@ module.exports = {
                 'revert',
                 'style',
                 'test',
-                'config'
+                'configs'
             ]
         ],
         'scope-enum': [
