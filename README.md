70649717ad4adaa4490fe2c9328861666752d775
Nathan GNANKADJA <gnankadjanathan@gmail.com>
2023-09-16 20:26:33 +0100

    config: [#2] setup conventional commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# inscriptum-blog-platform
+# Inscriptum Blog Platform
+
+Inscriptum is an open-source blog where anyone can come and share content.
+
+This Github repository contains the entire code base of the project.
+
+## Conventional
---
+++ commitlint.config.ts
@@ -0,0 +1,46 @@
+/**
+ * @see https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
+ */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-min-length': [
+            2,
+            'always',
+            10
+        ],
+        'header-max-length': [
+            2,
+            'always',
+            150
+        ],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+                'config'
+            ]
+        ],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'dashboard',
+                'public-site',
+                'common-utilities',
+                'api'
+            ]
+        ],
+    }
+}
