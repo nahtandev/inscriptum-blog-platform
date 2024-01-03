17816c7003e70d209cf2cdbcf1f1f2e39f073945
Nathan GNANKADJA <gnankadjanathan@gmail.com>
2024-01-03 01:40:03 +0100

    feat(api): [#18] secret management (wip)

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from "@nestjs/core";
+import { config as initDotenv } from "dotenv";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const port = 3000;
+    initDotenv();
+    const port = process.env.LISTEN_PORT || 3000;
     const app = await NestFactory.create(AppModule);
     await app.listen(port);
     console.log(`Server is running on port ${port}`);
